491ca223857c4d46d30eebeea69f9d0a2e597a69
Liupetro <1605252y@gmail.com>
2026-08-16 16:05:01 +0300

    Fix: remove prune, keep full deps for Next build

--- amvera-start.cjs
+++ amvera-start.cjs
@@ -1,3 +1,6 @@
 ﻿const { execSync } = require('child_process');
-execSync('npx next build', { stdio: 'inherit' });
+const fs = require('fs');
+if (!fs.existsSync('.next')) {
+  execSync('npx next build', { stdio: 'inherit' });
+}
 execSync('npx next start -H 0.0.0.0 -p 3000', { stdio: 'inherit' });
